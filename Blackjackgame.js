f3d557d9f61136fe6dcec64ed9cc26b5055502a2
Natan Getachew <natispecial27@gmail.com>
2021-11-30 14:49:37 +0300

    third change

--- Blackjackgame.js
+++ Blackjackgame.js
@@ -1,14 +1,54 @@
-FirstCard = 10
-SecondCard = 7
-Sum = FirstCard + SecondCard
+Cards = []
+Sum = 0
 HasBlackjack = false
-IsAlive = true
+IsAlive = false
 Message = ""
 
 MessageEl = document.getElementById("message-el")
-console.log(MessageEl)
+//console.log(MessageEl)
+SumEl = document.getElementById("sum-el")
+//SumEl = document.querySelector("#sum-el") //another way of selecting HTML element 
+CardsEl = document.getElementById("cards-el")
+
+Player = {
+    name: "John",
+    chips: 145
+}
+PlayerEl = document.getElementById("player-status")
+PlayerEl.textContent = Player.name + ": $" + Player.chips
+
+function GetRandomCard() {
+    RandomNo = Math.floor(Math.random()*13) + 1
+    if (RandomNo > 10) {
+        return 10
+    } 
+    else if (RandomNo === 1) {
+        return 11
+    }
+    else {
+        return RandomNo
+    }
+}
 
 function StartGame() {
+    IsAlive = true
+    FirstCard = GetRandomCard()
+    SecondCard = GetRandomCard()
+    Cards = [FirstCard, SecondCard]
+    Sum = FirstCard + SecondCard
+    
+    RenderGame()
+}
+function RenderGame() {
+    
+    CardsEl.textContent = "Cards: "
+
+    for (let i = 0; i < Cards.length; i++) {
+        CardsEl.textContent += Cards[i] + " "
+    }
+
+    SumEl.textContent = "Sum: " + Sum
+
     if (Sum <= 20) {
         Message = "Do you want to draw a new card?"
     }
@@ -22,3 +62,11 @@ function StartGame() {
     }
     MessageEl.textContent = Message
 }
+function NewCard() {
+    if (IsAlive === true && HasBlackjack === false ) {
+        Card = GetRandomCard()
+        Sum += Card
+        Cards.push(Card)
+        RenderGame()
+    }
+} //3:58
